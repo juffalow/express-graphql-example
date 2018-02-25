a697ff0aa401d0e0c45e33bf59e25ae93210af01
rchvalbo <28923633+rchvalbo@users.noreply.github.com>
2018-02-25 09:21:21 -0500

    Update index.js

--- graphql/queries/index.js
+++ graphql/queries/index.js
@@ -4,41 +4,9 @@ import quotes from './quote/quotes.js';
 import author from './author/author.js';
 import authors from './author/authors.js';
 
-////////////////////////////////////////////////
-
-import genUser from './00-genUser/genUser.js';
-import genUsers from './00-genUser/genUsers.js';
-
-import candidate from './01-candidate/candidate.js';
-import candidates from './01-candidate/candidates.js';
-
-import employer from './02-employer/employer.js';
-import employers from './02-employer/employers.js';
-
-import photo from './03-photo/photo.js';
-import photos from './03-photo/photos.js';
-
-import experience from './04-experience/experience.js';
-import experiences from './04-experience/experiences.js';
-
-import leadership from './05-leadership/leadership.js';
-import leaderships from './05-leadership/leaderships.js';
-
 export default {
     quote,
     quotes,
     author,
-    authors,
-    genUser,
-    genUsers,
-    candidate,
-    candidates,
-    employer,
-    employers,
-    photo,
-    photos,
-    experience,
-    experiences,
-    leadership,
-    leaderships,
+    authors
 };
