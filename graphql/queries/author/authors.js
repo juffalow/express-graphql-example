da741efa5247e4da4951fd2cb31ba114fd57e6d9
Matej Jellus <juffalow@juffalow.com>
2018-05-21 14:20:44 +0200

    Add pagination support for authors and quotes query
    Resolves #9

--- graphql/queries/author/authors.js
+++ graphql/queries/author/authors.js
@@ -1,19 +1,28 @@
 import {
   GraphQLList,
   GraphQLID,
-  GraphQLString,
-  GraphQLNonNull
+  GraphQLInt,
 } from 'graphql';
 
 import models from '../../../models/index.js';
 import Author from '../../types/author.js';
 
 export default {
-    type: new GraphQLList(Author),
-    args: {
-
+  type: new GraphQLList(Author),
+  args: {
+    first: {
+      type: GraphQLInt,
+      description: 'Limits the number of results returned in the page. Defaults to 10.',
     },
-    resolve(root, args) {
-        return models.author.findAll({where: args});
-    }
+    offset: {
+      type: GraphQLInt,
+    },
+  },
+  resolve(root, args) {
+    const offset = args.offset || 0;
+    const limit = args.first || 10;
+    delete args.offset;
+    delete args.first;
+    return models.author.findAll({where: args, offset, limit});
+  }
 };
--- graphql/queries/quote/quotes.js
+++ graphql/queries/quote/quotes.js
@@ -1,21 +1,31 @@
 import {
   GraphQLList,
   GraphQLID,
-  GraphQLString,
-  GraphQLNonNull
+  GraphQLInt,
 } from 'graphql';
 
 import models from '../../../models/index.js';
 import Quote from '../../types/quote.js';
 
 export default {
-    type: new GraphQLList(Quote),
-    args: {
-        author_id: {
-            type: GraphQLID
-        }
+  type: new GraphQLList(Quote),
+  args: {
+    author_id: {
+      type: GraphQLID,
     },
-    resolve(root, args) {
-        return models.quote.findAll({where: args, include: [ { model: models.author } ] });
-    }
+    first: {
+      type: GraphQLInt,
+      description: 'Limits the number of results returned in the page. Defaults to 10.',
+    },
+    offset: {
+      type: GraphQLInt,
+    },
+  },
+  resolve(root, args) {
+    const offset = args.offset || 0;
+    const limit = args.first || 10;
+    delete args.offset;
+    delete args.first;
+    return models.quote.findAll({where: args, include: [ { model: models.author } ], offset, limit });
+  }
 };
